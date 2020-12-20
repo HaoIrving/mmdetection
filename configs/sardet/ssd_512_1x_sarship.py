769e49320a9173cebb26bed1c3a3aaff1784a76b
sun <sun@sun>
2020-12-20 20:15:19 +0800

    lr 02

--- configs/sardet/ssd_512_1x_sarship.py
+++ configs/sardet/ssd_512_1x_sarship.py
@@ -125,8 +125,8 @@ data = dict(
         pipeline=test_pipeline))
 evaluation = dict(interval=20, metric='bbox')
 # optimizer
-# lr = 2e-3
-lr = 1e-3
+lr = 2e-3
+# lr = 1e-3
 optimizer = dict(type='SGD', lr=lr, momentum=0.9, weight_decay=5e-4)
 optimizer_config = dict()
 lr_config = dict(
