1e022d37c0af316747a39b1f1c3fc3ce4a2540ef
sun <sun@sun>
2020-12-21 09:52:42 +0800

    lr 1e3 / 2, bs 32, epc 300

--- configs/sardet/ssd_512_1x_sarship.py
+++ configs/sardet/ssd_512_1x_sarship.py
@@ -126,8 +126,8 @@ data = dict(
 evaluation = dict(interval=20, metric='bbox')
 # optimizer
 # lr = 2e-3
-lr = 1e-3
-total_epochs = 300 * 2
+lr = 1e-3 / 2
+total_epochs = 300
 optimizer = dict(type='SGD', lr=lr, momentum=0.9, weight_decay=5e-4)
 optimizer_config = dict()
 lr_config = dict(
