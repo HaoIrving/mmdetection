7f5cf576f12c91020409bbd94b61da0403ca05cc
sun <sun@sun>
2021-01-01 09:52:12 +0800

    db

--- tools/run.sh
+++ tools/run.sh
@@ -1,8 +1,8 @@
 cd ..
 
-bash ./tools/dist_train.sh \
-    ./configs/sardet/cascade_r101_dcn_1x_sarship.py  \
-    4 \
+# bash ./tools/dist_train.sh \
+#     ./configs/sardet/cascade_r101_dcn_1x_sarship.py  \
+#     4 \
 
 # git pull origin master
 
@@ -10,9 +10,9 @@ bash ./tools/dist_train.sh \
 #     ./configs/sardet/cascade_r101_dcn_1x_sarship.py  \
 #     4 \
 
-# bash ./tools/dist_train.sh \
-#     ./configs/sardet/ssd_512_1x_sarship.py  \
-#     4 \
+bash ./tools/dist_train.sh \
+    ./configs/sardet/ssd_512_1x_sarship.py  \
+    4 \
    
 # --resume-from ./work_dirs/ssd_512_1x_sarship/epoch_265.pth
 
@@ -25,3 +25,4 @@ bash ./tools/dist_train.sh \
 # log
 # scp -i qiaohong~qiaohong_sar -P 25392 root@172.18.41.31:/root/data/SAR/mmdetection/work_dirs/ssd_512_1x_sarship/20201221_103501.log.json /home/sun/projects/mmdetection/work_dirs
 # scp -i qiaohong~qiaohong_sar -P 25392 root@172.18.41.31:/root/data/SAR/mmdetection/work_dirs/cascade_r101_dcn_1x_sarship/20201222_013502.log.json /home/sun/projects/mmdetection/work_dirs
+python ./tools/analyze_logs.py plot_curve work_dirs/ssd_512_1x_sarship/20201230_141152.log.json  --keys bbox_mAP_50 bbox_mAP_s bbox_mAP_m bbox_mAP_l --legend bbox_mAP_50 bbox_mAP_s bbox_mAP_m bbox_mAP_l
